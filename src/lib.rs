917060e4cc2df5cd13a47ef3780cc981c3e0ba64
eightclound <879950144@qq.com>
2025-05-25 22:22:40 +0800

    first push

---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+use std::{error::Error, fs, path::PathBuf};
+
+use regex::Regex;
+
+pub fn read_txt_to_json(file_path: &std::path::PathBuf) -> Result<PathBuf, Box<dyn Error>> {
+    let txt = fs::read_to_string(file_path)?;
+
+    let mut disc_no = 0u32;
+    let disc_regex = Regex::new(r"^(\d+)\.$")?;
+    let movie_regex = Regex::new(r"^(\d{4})(.*?)((儿童))?$")?;
+    let mut movies = Vec::new();
+
+    for line in txt.lines().map(str::trim).filter(|l| ! l.is_empty()) {
+        if let Some(no) = disc_number(line, &disc_regex) {
+            disc_no = no;
+        } else {
+            if let Some(movie) = parse_movie(disc_no, line, &movie_regex) {
+                movies.push(movie);
+            } 
+        }
+    }
+    
+    save_to_json(movies)
+}
+
+fn save_to_json(movies: Vec<Movie>) -> Result<PathBuf, Box<dyn Error>> {
+    
+}
+
+fn parse_movie(disc_no: u32, line: &str, re: &Regex) -> Option<Movie> {
+    re.captures(line).map(|caps| {
+        println!("{caps:#?}");
+        Movie { 
+            disc: disc_no, 
+            year: caps.get(1).unwrap().as_str().trim().to_string(), 
+            title: caps.get(2).unwrap().as_str().trim().to_string(), 
+            remark: caps.get(3).map(|m|m.as_str().trim().to_string()), 
+        }
+    })
+}
+
+fn disc_number(line: &str, re: &Regex) -> Option<u32> {
+    re.captures(line)
+    .map(|caps| caps.get(1).unwrap().as_str().parse::<u32>().unwrap())
+}
+
+struct Movie {
+    disc: u32,
+    year: String,
+    title: String,
+    remark: Option<String>
+}
---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::{error::Error, process};
+use movie_importer::read_txt_to_json;
+use rfd::FileDialog;
+
+fn main() -> Result<(), Box<dyn Error>>{
+    match FileDialog::new().add_filter("Text Files", &["txt"])
+    .set_title("Select the DVD text file")
+    .set_directory("/")
+    .pick_file() {
+        Some(file_path) => {
+            let saved_path = read_txt_to_json(&file_path)?;
+            println!("Data saved to:{saved_path:?}");
+            Ok(())
+        }
+        None => {
+            eprintln!("File not selected.");
+            process::exit(-1)
+        }
+    }
+}
